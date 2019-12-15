c6be3b7d1d5f760807b7e3f5c6fb550388c3d6ca
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2019-12-15 17:42:58 +0530

    React StateManagement Functions Complete

--- src/Component/Photo.js
+++ src/Component/Photo.js
@@ -1,18 +1,16 @@
-import React,{Component} from 'react';
-class Photo extends Component{
-  
-    render(){
-         const post = this.props.post;
-        return <figure className="figure">
+import React from 'react';
+function Photo(props){
+    const post =props.post;
+    return <figure className="figure">
             <img src={post.imageLink} alt={post.description} className="photo"></img>
            <figcaption><p>{post.description}</p></figcaption> 
             <div className='button-container'>
                 <button className='remove-button'>
-Remove
+                 Remove
                 </button>
             </div>
 
         </figure>
-        }
 }
+
 export default Photo
