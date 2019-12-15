db57ca98775f0ed4ca3ade2e9d6d112d6a857974
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2019-12-15 19:37:07 +0530

    React StateManagement UpdateState Complete

--- src/Component/Photo.js
+++ src/Component/Photo.js
@@ -1,16 +1,22 @@
-import React from 'react';
-function Photo(props){
-    const post =props.post;
-    return <figure className="figure">
-            <img src={post.imageLink} alt={post.description} className="photo"></img>
-           <figcaption><p>{post.description}</p></figcaption> 
-            <div className='button-container'>
-                <button className='remove-button'>
-                 Remove
-                </button>
-            </div>
-
-        </figure>
+import React from "react";
+function Photo(props) {
+  const post = props.post;
+  return (
+    <figure className="figure">
+      <img src={post.imageLink} alt={post.description} className="photo"></img>
+      <figcaption>
+        <p>{post.description}</p>
+      </figcaption>
+      <div className="button-container">
+        <button
+          className="remove-button"
+          onClick={() => props.onRemovePhoto(post)}
+        >
+          Remove
+        </button>
+      </div>
+    </figure>
+  );
 }
 
-export default Photo
+export default Photo;
