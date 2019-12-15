6ae7777c05032a273560d6a932e42620887c4fa0
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2019-12-15 15:55:14 +0530

    React-StateManagement Component Architecture Complete

--- src/Component/Photo.js
+++ src/Component/Photo.js
@@ -2,8 +2,17 @@ import React,{Component} from 'react';
 class Photo extends Component{
   
     render(){
-        // const post = this.props.post;
-        return <figure className="figure"></figure>
+         const post = this.props.post;
+        return <figure className="figure">
+            <img src={post.imageLink} alt={post.description} className="photo"></img>
+           <figcaption><p>{post.description}</p></figcaption> 
+            <div className='button-container'>
+                <button className='remove-button'>
+Remove
+                </button>
+            </div>
+
+        </figure>
         }
 }
 export default Photo
