739fce581300ada7fadcc33d767d12fe81cde02d
dhruvpathak9305 <dhruvpathak9305@gmail.com>
2019-12-17 23:17:09 +0530

    Redux ReduxStore Reducer Complete

--- src/index.js
+++ src/index.js
@@ -3,5 +3,9 @@ import ReactDOM from "react-dom";
 import Main from "./Component/Main";
 import "./styles/stylesheet.css";
 import {BrowserRouter} from 'react-router-dom'
+import {createStore} from 'redux';
+import rootReducer from './redux/reducer';
+
+const store=createStore();
 
 ReactDOM.render(<BrowserRouter><Main /></BrowserRouter>, document.getElementById("root"));
